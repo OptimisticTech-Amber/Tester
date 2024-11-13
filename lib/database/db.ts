d14795e41f93264de29a222ea598e80296bfe763
OptimisticTech-Amber <amberkhan42266@gmail.com>
2024-11-13 22:27:49 +0530

    made changes in db

--- lib/database/db.ts
+++ lib/database/db.ts
@@ -1,32 +1,34 @@
-import mongoose,{ Mongoose } from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 
-const MONGO_DB_URL=process.env.MONGO_DB_URL;
+const MONGO_DB_URL = process.env.MONGO_DB_URL;
 
 interface MongooseConnection {
-    conn: Mongoose | null;
+  conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = (global as any).mongoose
+let cached: MongooseConnection = (global as any).mongoose;
 
-if(!cached) {
-    cached = (global as any).mongoose = { 
-      conn: null, promise: null 
-    }
-  }
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
 
-  export const connectToDatabase = async () => {
-    if(cached.conn) return cached.conn;
-  
-    if(!MONGO_DB_URL) throw new Error('Missing MONGODB_URL');
-  
-    cached.promise = 
-      cached.promise || 
-      mongoose.connect(MONGO_DB_URL, { 
-        dbName: 'AdeptAI', bufferCommands: false 
-      })
-  
-    cached.conn = await cached.promise;
-  
-    return cached.conn;
-  }
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!MONGO_DB_URL) throw new Error("Missing MONGODB_URL");
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGO_DB_URL, {
+      dbName: "AdeptAI",
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+};
